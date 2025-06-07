21276ae94b872c5ad3c539d5c6ea7d7addbfcd0c
ahmadsharifianzade <a.sharifianzade@gmail.com>
2025-06-07 11:46:31 +0330

    Add dynamic form builder

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Form, Question, Option, Response, Answer
+
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 1
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+    list_display = ("text", "type", "form")
+
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ("form", "submitted_at")
+    date_hierarchy = "submitted_at"
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("response", "question", "text", "option", "rating")
+    list_filter = ("question__type",)
+
--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from .forms import DynamicResponseForm
+from .models import Answer, Form as SurveyForm, Question, Response
+
+
+def take_form(request, form_id):
+    form_obj = get_object_or_404(SurveyForm.objects.prefetch_related("questions__options"), pk=form_id)
+    if request.method == "POST":
+        form = DynamicResponseForm(request.POST, form_obj=form_obj)
+        if form.is_valid():
+            response = Response.objects.create(form=form_obj)
+            for q in form_obj.questions.all():
+                key = f"question_{q.id}"
+                data = form.cleaned_data.get(key)
+                if q.type == Question.TEXT:
+                    Answer.objects.create(response=response, question=q, text=data)
+                elif q.type == Question.MULTIPLE_CHOICE:
+                    Answer.objects.create(response=response, question=q, option_id=int(data))
+                elif q.type == Question.RATING:
+                    Answer.objects.create(response=response, question=q, rating=int(data))
+            return redirect("crawler:thanks")
+    else:
+        form = DynamicResponseForm(form_obj=form_obj)
+    return render(request, "crawler/form.html", {"form": form, "form_obj": form_obj})
+
+
+def thanks(request):
+    return render(request, "crawler/thanks.html")
